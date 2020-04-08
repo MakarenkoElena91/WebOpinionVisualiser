a5aa79d0d2552a763a70280089bedc0d02263a25
MakarenkoElena91 <55669273+MakarenkoElena91@users.noreply.github.com>
2020-04-08 02:24:18 +0100

    read files

--- wcloud/WEB-INF/classes/ie.gmit.sw/Runner.java
+++ wcloud/WEB-INF/classes/ie.gmit.sw/Runner.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 public class Runner {
 
     public static void main(String[] args) throws IOException {
+        System.out.println("File path: " + new File("\"ignorewords.txt\"").getAbsolutePath());
+
+
         Search search = new Search();
 //        search.getSearchResults("Galway city");
         search.hardcoded("Galway");
